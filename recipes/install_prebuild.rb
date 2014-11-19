ae4e99761d3a8b00dcf753b4a5c310bffa6d1899
Bill Warner <bwarner@demark.com>
2014-11-19 13:38:52 -0700

    rename agent_ fiels to install_ to be more clear

---
+++ recipes/install_prebuild.rb
@@ -0,0 +1,26 @@
+# Author:: Nacer Laradji (<nacer.laradji@gmail.com>)
+# Cookbook Name:: zabbix
+# Recipe:: agent_prebuild
+#
+# Copyright 2011, Efactures
+#
+# Apache 2.0
+#
+
+include_recipe 'zabbix-agent::agent_common'
+
+# Install prerequisite RPM
+package 'redhat-lsb' if node['platform_family'] == 'rhel'
+
+ark 'zabbix_agent' do
+  name 'zabbix'
+  url node['zabbix']['agent']['prebuild']['url']
+  owner node['zabbix']['agent']['user']
+  group node['zabbix']['agent']['group']
+  action :put
+  path '/opt'
+  strip_components 0
+  has_binaries ['bin/zabbix_sender', 'bin/zabbix_get', 'sbin/zabbix_agent', 'sbin/zabbix_agentd']
+  notifies :restart, 'service[zabbix_agentd]'
+  checksum node['zabbix']['agent']['checksum']
+end
