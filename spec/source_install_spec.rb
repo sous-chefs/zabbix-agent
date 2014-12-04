d3fed6fb9e4c77bdadf2c337ce6d49f04d13269e
Bill Warner <bwarner@demark.com>
2014-12-04 07:55:49 -0700

    add some unit tests for the new download and compile resources

--- spec/source_install_spec.rb
+++ spec/source_install_spec.rb
@@ -17,4 +17,13 @@ describe 'zabbix-agent with default settings + source install' do
   it 'includes zabbix-agent::install_source to compile and install the zabbix agent from source' do
     expect(chef_run).to include_recipe('zabbix-agent::install_source')
   end
+
+  it "gets the file #{Chef::Config[:file_cache_path]}/zabbix-2.2.7.tar.gz" do
+    expect(chef_run).to create_remote_file("#{Chef::Config[:file_cache_path]}/zabbix-2.2.7.tar.gz")
+  end
+
+  it 'notifies the install_program script to run immediately' do
+    get_file = chef_run.remote_file("#{Chef::Config[:file_cache_path]}/zabbix-2.2.7.tar.gz")
+    expect(get_file).to notify('bash[install_program]').to(:run).immediately
+  end
 end
