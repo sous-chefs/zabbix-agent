536bac0772dab114cf6c47dc22c4b96484169f81
Bill Warner <bwarner@demark.com>
2014-12-19 12:47:26 -0700

    bump version and set dependancy to pass tests

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bill.warner@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Zabbix Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.9.0'
+version '0.10.0'
 supports 'ubuntu', '>= 10.04'
 supports 'debian', '>= 6.0'
 supports 'redhat', '>= 5.0'
@@ -16,5 +16,5 @@ supports 'windows'
 depends 'apt'           # For Debian family OSs
 depends 'yum'        # For Redhat family OSs
 depends 'build-essential' # for source install
-recommends 'ark'        # to install the prebuild packages
+depends 'ark'        # to install the prebuild packages
 recommends 'chocolatey' # For windows
