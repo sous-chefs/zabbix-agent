e93f0083a346264fe92c25ac2af38c757fb9edf5
Bill Warner <bwarner@demark.com>
2015-04-12 11:18:44 -0700

    updated supports bug to insure supported platforms is populated correctly

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,12 @@ description 'Installs/Configures Zabbix Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.13.0'
 
+# tested on ubuntu 10.04 12.04 and 14.04
 supports 'ubuntu', '>= 10.04'
-supports 'ubuntu', '>= 12.04'
-supports 'ubuntu', '>= 14.04'
+# tested on centos 6.6 and 7.0
 supports 'centos', '>= 6.6'
-supports 'centos', '>= 7.0'
+# tested on debian 6.0.10 and 7.0
 supports 'debian', '>= 6.0.10'
-supports 'debian', '>= 7.0'
 
 # change the needed recommends to depends below
 depends 'apt'             # For Debian family OSs
