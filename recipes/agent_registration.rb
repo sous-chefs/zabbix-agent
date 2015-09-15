549947d4c23b0b9d792ba713db9b117fc1d9f99c
Bill Warner <bwarner@demark.com>
2015-09-15 07:33:46 -0700

    fix rendered templates test

--- recipes/agent_registration.rb
+++ recipes/agent_registration.rb
@@ -5,18 +5,6 @@
 # Apache 2.0
 #
 
-if !Chef::Config[:solo]
-  zabbix_server = search(:node, 'recipes:zabbix\:\:server').first
-else
-  if node['zabbix']['web']['fqdn']
-    zabbix_server = node
-  else
-    Chef::Log.warn('This recipe uses search. Chef Solo does not support search.')
-    Chef::Log.warn("If you did not set node['zabbix']['web']['fqdn'], the recipe will fail")
-    return
-  end
-end
-
 connection_info = {
   url: "http://#{zabbix_server['zabbix']['web']['fqdn']}/api_jsonrpc.php",
   user: zabbix_server['zabbix']['web']['login'],
@@ -72,7 +60,7 @@ interface_list.each do |interface|
   end
 end
 
-zabbix_host node['hostname'] do
+libzabbix_host node['hostname'] do
   create_missing_groups true
   server_connection connection_info
   parameters(
