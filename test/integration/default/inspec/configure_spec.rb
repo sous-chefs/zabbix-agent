b9e06dd107fc19d3acd861261e93c175db618b16
Bill Warner <bwarner@demark.com>
2017-01-10 15:33:59 -0700

    port from old chefspec to new inspec integration tests

--- test/integration/default/inspec/configure_spec.rb
+++ test/integration/default/inspec/configure_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe file('/etc/zabbix/zabbix_agentd.conf') do
   it { should be_file }
 end
--- test/integration/default/inspec/service_spec.rb
+++ test/integration/default/inspec/service_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe service('zabbix_agentd') do
   it { should be_running }
 end
