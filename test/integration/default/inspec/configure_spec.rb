f7acd9c0bf739445b753fe7fb52fd20dc9d49720
Graham Davison <gdavison@paybyphone.com>
2017-05-01 14:33:05 -0700

    Updated Windows support

--- test/integration/default/inspec/configure_spec.rb
+++ test/integration/default/inspec/configure_spec.rb
@@ -1,3 +1,9 @@
-describe file('/etc/zabbix/zabbix_agentd.conf') do
+config_file = if os.windows?
+                'C:\programdata\zabbix\zabbix_agentd.conf'
+              else
+                '/etc/zabbix/zabbix_agentd.conf'
+              end
+
+describe file(config_file) do
   it { should be_file }
 end
