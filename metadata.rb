6162aa282db65a35c8ea1008b441d2363e972988
Tim Smith <tsmith84@gmail.com>
2019-08-17 14:47:02 -0700

    Remove long description field from metadata
    This is pointless
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs/Configures Zabbix Agent'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url        'https://github.com/sous-chefs/zabbix-agent'
 issues_url        'https://github.com/sous-chefs/zabbix-agent/issues'
 version           '0.14.1'
